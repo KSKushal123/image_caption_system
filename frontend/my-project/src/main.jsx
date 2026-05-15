f77285f28ef7b35718f7d932f87d7f8d098dae4d
KSKushal123 <kskushal123456@gmail.com>
2026-05-15 12:23:50 +0530

    saz

--- frontend/my-project/src/main.jsx
+++ frontend/my-project/src/main.jsx
@@ -3,9 +3,11 @@ import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 
-import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import LandingPage from "./pages/LandingPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const router = createBrowserRouter([
   {
@@ -24,6 +26,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Header/>
     <RouterProvider router={router} />
+    <Footer/>
   </React.StrictMode>
 );
